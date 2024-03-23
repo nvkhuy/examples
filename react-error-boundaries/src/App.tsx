b7f43a884ed83a4f6ece0f322493468d0f9cb679
huy <huy@192.168.2.4>
2024-03-23 17:59:27 +0700

    update

--- react-error-boundaries/src/App.tsx
+++ react-error-boundaries/src/App.tsx
@@ -1,24 +1,29 @@
 import "./App.css";
-import { UsingTheRightWay } from "./errorHanlding/usingTheRightWay";
-import { ErrorBoundary } from "react-error-boundary";
-import { ProductsFetchingError } from "./components/errors/ErrorBoundrayComponent";
+import {UsingTheRightWay} from "./errorHanlding/usingTheRightWay";
+import {ErrorBoundary} from "react-error-boundary";
+import {ProductsFetchingError} from "./components/errors/ErrorBoundrayComponent";
+import {StandardErrorBoundary, UsingErrorBoundaries} from "./errorHanlding/usingErrorBoundaries";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <UsingState /> */}
-      {/* <StandardErrorBoundary>
+    return (
+        <div className="App">
+            {/* <UsingState /> */}
+            {/* <StandardErrorBoundary>
         <UsingErrorBoundaries />
       </StandardErrorBoundary> */}
 
-      <ErrorBoundary
-        FallbackComponent={ProductsFetchingError}
-        onError={() => console.log("Error happened!")}
-      >
-        <UsingTheRightWay />
-      </ErrorBoundary>
-    </div>
-  );
+            <StandardErrorBoundary>
+                <UsingErrorBoundaries/>
+            </StandardErrorBoundary>
+
+            {/*<ErrorBoundary*/}
+            {/*  FallbackComponent={ProductsFetchingError}*/}
+            {/*  onError={() => console.log("Error happened!")}*/}
+            {/*>*/}
+            {/*  <UsingTheRightWay />*/}
+            {/*</ErrorBoundary>*/}
+        </div>
+    );
 }
 
 export default App;
