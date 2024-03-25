d0985aeef577f44db830ab7f35794e2fca1026d9
huy <huy@huys-MacBook-Pro.local>
2024-03-25 09:34:53 +0700

    add react-context-api example

---
+++ react-context-api/src/Button.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import { ThemeContext } from './store/ThemeContext'
+import './App.css'
+
+const Button = () => {
+  const { isDarkTheme, toggleTheme } = useContext(ThemeContext)
+  return (
+    <button onClick={toggleTheme} className={`${isDarkTheme ? 'lightBtn' : 'darkBtn'}`}>
+      change Theme
+    </button>
+  )
+}
+
+export default Button
