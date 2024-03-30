6074f93fb3b1c84ae941a1cf018ce8cedd9b0f4f
nvkhuy <huy@192.168.2.4>
2024-03-30 14:52:55 +0700

    add seperate error

--- react-suspense/src/pages/_app.js
+++ react-suspense/src/pages/_app.js
@@ -1,56 +1,58 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import { SWRConfig } from "swr";
+import {useEffect, useState} from "react";
+import {SWRConfig} from "swr";
 import "tailwindcss/tailwind.css";
 import "../../mirage";
+import Script from "next/script";
 
 let isClient = typeof window !== "undefined";
 
 export default function _app(props) {
-  let [isFirstRender, setIsFirstRender] = useState(true);
+    let [isFirstRender, setIsFirstRender] = useState(true);
 
-  useEffect(() => {
-    setIsFirstRender(false);
-  }, []);
+    useEffect(() => {
+        setIsFirstRender(false);
+    }, []);
 
-  return (
-    <>
-      <Head>
-        {process.env.NODE_ENV !== "production" && (
-          <script
-            dangerouslySetInnerHTML={{ __html: noOverlayWorkaroundScript }}
-          />
-        )}
-      </Head>
+    return (
+        <>
+            <Head>
+                {process.env.NODE_ENV !== "production" && (
+                    <script
+                        dangerouslySetInnerHTML={{__html: noOverlayWorkaroundScript}}
+                    />
+                )}
+            </Head>
 
-      {isClient && !isFirstRender ? (
-        <SWRConfig
-          value={{
-            fetcher: (...args) =>
-              fetch(...args).then((res) => {
-                if (res.ok) {
-                  return res.json();
-                } else {
-                  throw new Error("Fetch failed");
-                }
-              }),
-          }}
-        >
-          <AppInner {...props} />
-        </SWRConfig>
-      ) : null}
-    </>
-  );
+            {isClient && !isFirstRender ? (
+                <SWRConfig
+                    value={{
+                        fetcher: (...args) =>
+                            fetch(...args).then((res) => {
+                                if (res.ok) {
+                                    return res.json();
+                                } else {
+                                    throw new Error("Fetch failed");
+                                }
+                            }),
+                    }}
+                >
+                    <AppInner {...props} />
+                </SWRConfig>
+            ) : null}
+        </>
+    );
 }
 
-function AppInner({ Component, pageProps }) {
-  return (
-    <div className="flex justify-center w-full min-h-screen pt-8 antialiased xs:px-20 sm:px-10 md:pt-28 bg-slate-100">
-      <div className="w-full max-w-sm px-8">
-        <Component {...pageProps} />
-      </div>
-    </div>
-  );
+function AppInner({Component, pageProps}) {
+    return (
+        <div
+            className="flex justify-center w-full min-h-screen pt-8 antialiased xs:px-20 sm:px-10 md:pt-28 bg-slate-100">
+            <div className="w-full max-w-sm px-8">
+                <Component {...pageProps} />
+            </div>
+        </div>
+    );
 }
 
 const noOverlayWorkaroundScript = `
--- react-suspense/src/pages/index.js
+++ react-suspense/src/pages/index.js
@@ -1,5 +1,5 @@
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
+import {Suspense} from "react";
+import {ErrorBoundary} from "react-error-boundary";
 import Card from "../components/card";
 import Error from "../components/error";
 import Stat from "../components/stat";
@@ -7,41 +7,51 @@ import * as Icons from "@heroicons/react/outline";
 import Spinner from "../components/spinner";
 
 export default function Home() {
-  return (
-    <>
-      <h3 className="mt-2 mb-5 text-lg font-medium leading-6 text-slate-900">
-        Your accounts
-      </h3>
+    return (
+        <>
+            <h3 className="mt-2 mb-5 text-lg font-medium leading-6 text-slate-900">
+                Your accounts
+            </h3>
 
-      <ErrorBoundary fallback={<Error>Could not fetch data.</Error>}>
-        <Suspense fallback={<Spinner />}>
-          <div className="grid grid-cols-1 gap-5">
-            <Card>
-              <Stat
-                label="Checking"
-                endpoint="/api/checking"
-                Icon={Icons.CashIcon}
-              />
-            </Card>
+            <div className="grid grid-cols-1 gap-5">
+                <ErrorBoundary fallback={<Error>Could not fetch data.</Error>}>
+                    <Suspense fallback={<Spinner/>}>
+                        <Card>
+                            <Stat
+                                label="Checking"
+                                endpoint="/api/checking"
+                                Icon={Icons.CashIcon}
+                            />
+                        </Card>
+                    </Suspense>
+                </ErrorBoundary>
 
-            <Card>
-              <Stat
-                label="Savings"
-                endpoint="/api/savings"
-                Icon={Icons.CurrencyDollarIcon}
-              />
-            </Card>
+                <ErrorBoundary fallback={<Error>Could not fetch data.</Error>}>
+                    <Suspense fallback={<Spinner/>}>
+                        <Card>
+                            <Stat
+                                label="Savings"
+                                endpoint="/api/savings"
+                                Icon={Icons.CurrencyDollarIcon}
+                            />
+                        </Card>
+                    </Suspense>
+                </ErrorBoundary>
 
-            <Card>
-              <Stat
-                label="Credit Card"
-                endpoint="/api/credit"
-                Icon={Icons.CreditCardIcon}
-              />
-            </Card>
-          </div>
-        </Suspense>
-      </ErrorBoundary>
-    </>
-  );
+                <ErrorBoundary fallback={<Error>Could not fetch data.</Error>}>
+                    <Suspense fallback={<Spinner/>}>
+                        <Card>
+                            <Stat
+                                label="Credit Card"
+                                endpoint="/api/credit"
+                                Icon={Icons.CreditCardIcon}
+                            />
+                        </Card>
+                    </Suspense>
+                </ErrorBoundary>
+            </div>
+
+        </>
+    )
+        ;
 }
